d49e8a28752e54ac2755f25f8398d08d9d8c89d5
noelfoka <nomgnenoel@gmail.com>
2023-09-28 17:54:47 +0100

    simple condition

--- exo1.rb
+++ exo1.rb
@@ -6,6 +6,25 @@ nom = gets.chomp
 puts "Salut #{nom} comment allez-vous"
 =end
 
+=begin
 puts "entrez un chiffre"
 chiffre = gets.chomp.to_i
-puts chiffre.even?
+if chiffre.even?
+    puts "Votre chiffre est paire"
+else
+    puts "Votre chiffre est impaire"
+end
+=end
+
+puts "Entrez un chiffre: "
+
+chiffre = gets.chomp.to_i
+adeviner = 15
+
+if chiffre > adeviner
+    puts "Chiffre trpo grand"
+elsif chiffre < adeviner
+    puts "Chiffre trop petit"
+else
+    puts "Bravooooo tu as devine"
+end
