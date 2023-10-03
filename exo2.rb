19f3a1bbaf0dec6e79d6637105afc62cede21201
noelfoka <nomgnenoel@gmail.com>
2023-10-03 01:01:20 +0100

    update

--- exo2.rb
+++ exo2.rb
@@ -2,7 +2,11 @@ x = 15
 y = 0
 
 while x != y
-    puts "entrez un autre chiffre"
     y = gets.chomp.to_i
+    if y < x
+        puts "Le chiffre est trop petit"
+    elsif y > x
+        puts "le chiffre est trop grand"
+    end
 end
 puts "Vous avez deviné le chiffre"
