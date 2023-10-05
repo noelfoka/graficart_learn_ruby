e8acd62880f1dd15629eff84741a9544aa251568
noelfoka <nomgnenoel@gmail.com>
2023-10-05 15:18:49 +0100

    learn iterator

---
+++ exo_iterator.rb
@@ -0,0 +1,18 @@
+# First itterator: Loop
+
+adeviner = 10
+a = 0
+
+loop do
+    puts "entrez un chiffre"
+    a = gets.chomp.to_i
+
+    if a < adeviner
+        puts "chiffre trop petit"
+    elsif a > adeviner
+        puts "chiffre trop grand"
+    else
+        puts "Bravoooo tu as devine"
+        break
+    end
+end
