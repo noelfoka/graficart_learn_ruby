11f2850b0153665d1d8024b2d09c8f787f600009
noelfoka <nomgnenoel@gmail.com>
2023-10-11 13:00:37 +0100

    learn method

---
+++ methode.rb
@@ -0,0 +1,15 @@
+def convert_to_min(seconde)
+  seconde / 60
+end
+
+puts convert_to_min(4000)
+
+def bonjour(nom)
+    return "Bonjour #{nom}"
+end
+
+puts bonjour('Jean')
+
+def salutation(prefixe, nom)
+    "#{salut} #{Jean}"
+end
