557b37d0ee88776e4804f4110e2fcef3072e8874
noelfoka <nomgnenoel@gmail.com>
2023-10-12 07:23:33 +0100

    update

--- exo_procblock.rb
+++ exo_procblock.rb
@@ -1,3 +1,27 @@
+=begin
 tableau = [2, 5, 3, 10]
 
-puts tableau.map { |item| item * 2 }
+tb = tableau.map { |item| item * 2 }
+
+puts tb.inspect
+=end
+
+eleves = [
+  {nom: 'Joel', note: 18},
+  {nom: 'Marc', note: 8},
+  {nom: 'Julie', note: 12},
+  {nom: 'Ismael', note: 15},
+  {nom: 'Inco', note: 4}
+]
+
+def lamoyenne(eleves)
+  eleves.each do |eleve|
+    if eleve[:note] >= 10
+      yield(eleve)
+    end
+  end
+end
+
+lamoyenne(eleves) do |eleve|
+  puts "#{eleve[:nom]} a la moyenne"
+end
